43f014c2b13ea94ad8bb701a274d4720f6eea2bc
rickyalfin <rickyalfina30@gmail.com>
2025-07-06 22:53:08 +0700

    Membuat Tampilan Error

---
+++ lib/core/widget/error_app_widget.dart
@@ -0,0 +1,42 @@
+import 'package:presensi_pegawai_flutter/core/helper/global_helper.dart';
+import 'package:flutter/material.dart';
+
+class ErrorAppWidget extends StatelessWidget {
+  final String description;
+  final void Function() onPressDefaultButton;
+  final FilledButton? alternatifButton;
+  const ErrorAppWidget({
+    super.key,
+    required this.description,
+    required this.onPressDefaultButton,
+    required this.alternatifButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.error, size: 100),
+          SizedBox(height: 20),
+          Text(
+            description,
+            style: GlobalHelper.getTextStyle(
+              context,
+              appTextStyle: AppTextStyle.HEADLINE_SMALL,
+            ),
+          ),
+          SizedBox(height: 30),
+          alternatifButton ??
+              FilledButton.icon(
+                onPressed: onPressDefaultButton,
+                icon: Icon(Icons.refresh),
+                label: Text("Refresh"),
+              ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:presensi_pegawai_flutter/core/di/dependency.dart';
+import 'package:presensi_pegawai_flutter/core/widget/error_app_widget.dart';
 import 'package:presensi_pegawai_flutter/core/widget/loading_app_widget.dart';
 
 void main() async {
@@ -16,7 +17,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: LoadingAppWidget()),
+      home: Scaffold(
+        body: ErrorAppWidget(
+          description: "Error request API",
+          onPressDefaultButton: () {
+            print("refresh button on click");
+          },
+          alternatifButton: FilledButton(
+            child: Text("Print"),
+            onPressed: () {
+              print("Print Sekarang");
+            },
+          ),
+        ),
+      ),
     );
   }
 }
